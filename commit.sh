5cbde1dafb0f84318c334ee51a5d238028d36c3b
Chawalit Sengsang <chawalit1987@gmail.com>
2022-09-19 10:37:58 +0700

    Edit git sh

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 echo "====== Start git command ======"
 git add .
-echo "git add all"
+echo "Step (1): git add all"
 git commit -m "$1"
-echo "git commit $1"
+echo "Step (2): git commit $1"
 git push -u origin master
-echo "git push origin master"
+echo "Step (3): git push origin master"
 echo "====== End git command ======"
